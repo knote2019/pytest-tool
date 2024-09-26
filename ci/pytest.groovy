316fd25750a53b8c939d703eebb4312786d6e21e
minghong.kang <minghong.kang@iluvatar.com>
2024-09-26 20:05:11 +0800

    fx

--- ci/pytest.groovy
+++ ci/pytest.groovy
@@ -64,7 +64,7 @@ pipeline {
         stage('generate_report') {
             steps {
                 script{
-                    exec_shell """mkdir iluvatar_test_report; mv \$(find . -name iluvatar_test_report.xml) iluvatar_test_report"""
+                    exec_shell """find . -name iluvatar_test_report"""
                     allure([includeProperties: false, jdk: '', results: [[path: 'iluvatar_test_report']]])
                 }
             }
