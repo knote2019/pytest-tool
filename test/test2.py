d91e1d335ef41ff8193274eaa9d3bd3fc4fb2d80
kasmvnc <kasmvnc@iluvatar.com>
2024-09-26 13:08:11 +0800

    fx

--- test/test2.py
+++ test/test2.py
@@ -12,7 +12,7 @@ parameters_list = [
 ]
 
 
-class TestCase1(TestCaseBase):
+class TestCase2(TestCaseBase):
     @pytest.mark.parametrize("parameters", parameters_list)
     def testcase1(self, parameters):
         print(parameters.tp_size)
---
+++ test/test3.py
@@ -0,0 +1,27 @@
+from collections import namedtuple
+
+import pytest
+import torch
+
+from test.common.testcase_base import TestCaseBase
+
+Parameters = namedtuple('Parameters', ['tp_size', 'dtype', 'bias'])
+parameters_list = [
+    Parameters(1, torch.float16, True),
+    Parameters(2, torch.bfloat16, False),
+]
+
+
+class TestCase2(TestCaseBase):
+    @pytest.mark.parametrize("parameters", parameters_list)
+    def testcase1(self, parameters):
+        print(parameters.tp_size)
+        print(parameters.dtype)
+        print(parameters.bias)
+
+        a = torch.tensor([[0.1, 1.2], [3.4, 4.5], [6.7, 7.8]])
+        print(a)
+        self.save_tensor("a.pth", a)
+
+        b = self.load_tensor("a.pth")
+        print(b)
