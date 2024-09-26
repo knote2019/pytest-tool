8afe63ee73dac1ddfab22d69f1d9ec88983f91bb
kasmvnc <kasmvnc@iluvatar.com>
2024-09-26 23:08:24 +0800

    fx

--- ci/pytest.groovy
+++ ci/pytest.groovy
@@ -64,7 +64,7 @@ pipeline {
         stage('generate_report') {
             steps {
                 script{
-                    allure([includeProperties: false, jdk: '', results: [[path: 'iluvatar_test_report']]])
+                    allure([results: [[path: 'iluvatar_test_report']]])
                 }
             }
         }
