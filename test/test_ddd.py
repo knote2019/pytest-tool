9801cdff2a31dbaa120443ecfac48d52daacddc5
kasmvnc <kasmvnc@iluvatar.com>
2024-09-28 00:43:14 +0800

    fx

--- test/test_ddd.py
+++ test/test_ddd.py
@@ -16,9 +16,9 @@ parameters_list = [
 class TestDDD(TestCase):
     @pytest.mark.parametrize("parameters", parameters_list)
     def test_ddd(self, parameters):
-        mp.spawn(self.te_linear, nprocs=parameters.tp_size, join=True)
+        mp.spawn(self.rank_process, nprocs=parameters.tp_size, join=True)
 
-    def te_linear(self, rank):
+    def rank_process(self, rank):
         self.init_process_group(self.parameters.tp_size, rank)
         print("kkk")
         self.destroy_process_group()
