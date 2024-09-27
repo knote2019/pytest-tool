e7446166845336512febb0109a47581786d21828
kasmvnc <kasmvnc@iluvatar.com>
2024-09-27 14:43:10 +0800

    fx

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 CI_CASES_REGEX="$1"
 echo "CI_CASES_REGEX=${CI_CASES_REGEX}"
-
+# show current folder.
 pwd
 ls -l
-
+# run test.
 echo "pytest ${CI_CASES_REGEX} --capture=no --clean-alluredir --alluredir=./test_report"
 pytest ${CI_CASES_REGEX} --capture=no --clean-alluredir --alluredir=./test_report
-
+# show current folder.
 ls -l ${CI_TEST_REPORT_PATH}
