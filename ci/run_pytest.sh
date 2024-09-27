5ba8338578a755a818f15a2a2f291980e6154e7f
kasmvnc <kasmvnc@iluvatar.com>
2024-09-27 14:07:04 +0800

    fx

--- ci/run_pytest.sh
+++ ci/run_pytest.sh
@@ -10,9 +10,9 @@ echo "CI_CASES_REGEX=${CI_CASES_REGEX}"
 echo "CI_TEST_REPORT_PATH=${CI_TEST_REPORT_PATH}"
 
 if [[ ".py" =~ "${CI_CASES_REGEX}" ]];then
-    echo pytest ${CI_CASES_REGEX} --alluredir=${CI_TEST_REPORT_PATH}
-    pytest ${CI_CASES_REGEX} --alluredir=${CI_TEST_REPORT_PATH}
+    echo pytest ${CI_CASES_REGEX} --capture=no --clean-alluredir --alluredir=${CI_TEST_REPORT_PATH}
+    pytest ${CI_CASES_REGEX} --capture=no --clean-alluredir --alluredir=${CI_TEST_REPORT_PATH}
 else
-    echo pytest ${CI_CASES_REGEX}/*.py --alluredir=${CI_TEST_REPORT_PATH}
-    pytest ${CI_CASES_REGEX}/*.py --alluredir=${CI_TEST_REPORT_PATH}
+    echo pytest ${CI_CASES_REGEX}/*.py --capture=no --clean-alluredir --alluredir=${CI_TEST_REPORT_PATH}
+    pytest ${CI_CASES_REGEX}/*.py --capture=no --clean-alluredir --alluredir=${CI_TEST_REPORT_PATH}
 fi
