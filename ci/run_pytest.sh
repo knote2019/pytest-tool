660295fd359c9b676fd87cd38dd090e6ae9388e5
kasmvnc <kasmvnc@iluvatar.com>
2024-09-27 14:12:02 +0800

    fx

--- ci/run_pytest.sh
+++ ci/run_pytest.sh
@@ -9,5 +9,7 @@ CI_TEST_REPORT_PATH="$2"
 echo "CI_CASES_REGEX=${CI_CASES_REGEX}"
 echo "CI_TEST_REPORT_PATH=${CI_TEST_REPORT_PATH}"
 
+pwd
+ls -l
 echo pytest ${CI_CASES_REGEX} --capture=no --clean-alluredir --alluredir=${CI_TEST_REPORT_PATH}
 pytest ${CI_CASES_REGEX} --capture=no --clean-alluredir --alluredir=${CI_TEST_REPORT_PATH}
